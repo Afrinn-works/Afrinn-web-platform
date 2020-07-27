01c8c3da2c1f4d8311d744d182ad29da2c97d4e9
Daniel Bemsen <danireptor7@gmail.com>
2020-07-27 22:54:29 +0100

    setting up heroku servers

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+// Variables
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(express.static("public"));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/src/index.html')
+})
+
+
+app.listen(3000,  () => {
+    console.log(`Server started and running on port ${port}`);
+});
